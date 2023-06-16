800a79e1e753df9cea86378e2c77caecb4eb8afb
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-06-16 14:00:40 -0400

    Aplicación de estilos a pantalla celular

--- src/components/login.js
+++ src/components/login.js
@@ -1,45 +1,45 @@
 export const login = (onNavigate) => {
-
   const divLogin = document.createElement('div');
-  divLogin.className = "div-login";
+  divLogin.className = 'div-login';
 
   const formLogin = document.createElement('form');
-  formLogin.className = "form-login";
+  formLogin.className = 'form-login';
 
   const imageLogin = document.createElement('img');
-  imageLogin.className = "img-login";
-
+  imageLogin.className = 'img-login';
+  imageLogin.src = './images/LogoVBB.png';
+  imageLogin.alt = 'logo de Vegan Book';
+  formLogin.appendChild(imageLogin);
 
   const title = document.createElement('h2');
-  title.textContent = "Ingrese a su cuenta";
+  title.textContent = 'Ingrese a su cuenta';
   formLogin.appendChild(title);
 
   const email = document.createElement('input');
-  email.setAttribute("id", "inputprueba")
-  email.className = "input-email";
+  email.className = 'input-email';
   email.type = 'text';
-  email.placeholder = "Ingresa tu email";
-  email.value = "";
+  email.placeholder = 'Ingresa tu email';
+  email.value = '';
   formLogin.appendChild(email);
 
   const password = document.createElement('input');
-  password.className = "input-password";
+  password.className = 'input-password';
   password.type = 'password';
-  password.placeholder = "Contraseña";
-  password.value = "";
+  password.placeholder = 'Contraseña';
+  password.value = '';
   formLogin.appendChild(password);
 
   const buttonLogin = document.createElement('button');
-  buttonLogin.className = "button-login";
-  buttonLogin.textContent = "Iniciar Sesión";
+  buttonLogin.className = 'button-login';
+  buttonLogin.textContent = 'Iniciar Sesión';
   buttonLogin.type = 'submit';
   formLogin.appendChild(buttonLogin);
 
   buttonLogin.addEventListener('click', () => onNavigate('/')); //muro red social
 
   const buttonGoogle = document.createElement('button');
-  buttonGoogle.className = "button-google";
-  buttonGoogle.textContent = "Iniciar Sesión con Google";
+  buttonGoogle.className = 'button-google';
+  buttonGoogle.textContent = 'Iniciar Sesión con Google';
   buttonGoogle.type = 'submit';
   formLogin.appendChild(buttonGoogle);
 
@@ -49,12 +49,12 @@ export const login = (onNavigate) => {
   formLogin.appendChild(hr);
 
   const optionalText = document.createElement('h3');
-  optionalText.textContent = "¿Eres nuevo en VeganBook?";
+  optionalText.textContent = '¿Eres nuevo en VeganBook?';
   formLogin.appendChild(optionalText);
 
   const buttonRegister = document.createElement('button');
-  buttonRegister.className = "button-register";
-  buttonRegister.textContent = "¡Registrate!";
+  buttonRegister.className = 'button-register';
+  buttonRegister.textContent = '¡Registrate!';
   buttonRegister.type = 'submit';
   formLogin.appendChild(buttonRegister);
 
