666fe5d40ec4fac4ca0f20ddec659dc56d53fc49
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-06-27 16:49:37 -0600

    Primer test funcional para Register

--- test/register.spec.js
+++ test/register.spec.js
@@ -1,57 +1,41 @@
 import { createUser } from '../src/lib/firebase';
-// import register from '../src/components/register';
+import { register } from '../src/components/register';
 
 jest.mock('../src/lib/firebase');
 
-// function tick() {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, 0);
-//   });
-// }
-describe('createUser', () => {
-  it('registro de usuario', async () => {
-    const registroEjemplo = createUser('john@gmail.com', '123456');
-    await expect(registroEjemplo).resolves.toBeTruthy();
+function tick() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 0);
   });
-  //   //   let divRegister;
-  //   //   let formRegister;
-  //   let inputEmail;
-  //   let inputPassword;
-  //   let buttonCreateAccount;
-  //   let errorText;
-  //   beforeEach(() => {
-  //     // document.body.appendChild(register());
-  //     // divRegister = document.getElementByClassName('div-register');
-  //     // formRegister = document.getElementByClassName('form-register');
-  //     inputEmail = document.getElementById('email');
-  //     inputPassword = document.getElementById('password');
-  //     buttonCreateAccount = document.getElementByClassName(
-  //       'button-create-account'
-  //     );
-  //     errorText = document.getElementByClassName('error-text-register');
-  //   });
-  //   it('Debería mostrar un error si el correo de usuario ya está registrado', async () => {
-  //     register.mockImplementationOnce((email, password) => {
-  //       return Promise.reject(
-  //         new Error('Firebase: Error (auth/email-already-in-use).')
-  //       );
-  //     });
-  //     buttonCreateAccount.click();
-  //     await tick();
-  //     expect(errorText.innerHTML).toBe(
-  //       'Firebase: Error (auth/email-already-in-use).'
-  //     );
-  //   });
-  //   it('Debería mostrar exito', async () => {
-  //     register.mockImplementationOnce((email, password) => {
-  //       return Promise.resolve({
-  //         user: { userCredential: 12345, email: email },
-  //       });
-  //     });
-  //     inputEmail.value = 'email@verify.com';
-  //     inputPassword.value = '123456';
-  //     buttonCreateAccount.click();
-  //     await tick();
-  //     expect(createUser).toReturn(true);
-  //   });
+}
+describe('createUser', () => {
+  // let divRegister;
+  // let formRegister;
+  // let inputEmail;
+  // let inputPassword;
+  let buttonCreateAccount;
+  let errorText;
+  beforeEach(() => {
+    const onNavigateMock = () => {};
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+    document.body.appendChild(register(onNavigateMock));
+    // divRegister = document.getElementsByClassName('div-register')[0];
+    // formRegister = document.getElementsByClassName('form-register')[0];
+    // inputEmail = document.getElementById('email');
+    // inputPassword = document.getElementById('password');
+    buttonCreateAccount = document.getElementsByClassName('button-create-account')[0];
+    errorText = document.getElementsByClassName('error-text-register')[0];
+  });
+
+  it('Debería mostrar un error si los campos de texto están vacíos', async () => {
+    const error = new Error();
+    error.code = 'auth/email-already-exists';
+
+    createUser.mockImplementationOnce(() => Promise.reject(error));
+    buttonCreateAccount.click();
+    await tick();
+    expect(errorText.innerHTML).toBe('Ups 🙈, ingresa un correo y una contraseña');
+  });
 });
